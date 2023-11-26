cf7a9638ca0761f9c93a8fa2e6a67ee02cf03187
H-ymt <y.handai1272@gmail.com>
2023-11-26 18:43:34 +0900

    style: format by prettier

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,26 +1,39 @@
-import * as React from "react"
+import * as React from "react";
 
 export default function ThemeToggle() {
-  const [theme, setThemeState] = React.useState<"theme-light"| "dark" | "system">("theme-light")
+  const [theme, setThemeState] = React.useState<
+    "theme-light" | "dark" | "system"
+  >("theme-light");
 
   React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark")
-    setThemeState(isDarkMode ? "dark" : "theme-light")
-  }, [])
-
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setThemeState(isDarkMode ? "dark" : "theme-light");
+  }, []);
 
   React.useEffect(() => {
     const isDark =
       theme === "dark" ||
       (theme === "system" &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    document.documentElement.classList[isDark ? "add" : "remove"]("dark")
-  }, [theme])
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  }, [theme]);
 
   return (
-    <div>
-<button type="button" onClick={() => setThemeState("theme-light")} className='text-black dark:text-white'>Light</button>
-<button type="button" onClick={() => setThemeState("dark")} className='text-black dark:text-white'>Dark</button>
+    <div className="flex gap-2">
+      <button
+        type="button"
+        onClick={() => setThemeState("theme-light")}
+        className="text-black dark:text-white"
+      >
+        Light
+      </button>
+      <button
+        type="button"
+        onClick={() => setThemeState("dark")}
+        className="text-black dark:text-white"
+      >
+        Dark
+      </button>
     </div>
-  )
+  );
 }
